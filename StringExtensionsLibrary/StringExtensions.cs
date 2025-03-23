a111bffc9f96a8fe25ea6291277845b6a6c8fd61
Sarah Shields <sshield9@my.centennialcollege.ca>
2025-03-23 14:56:25 -0400

    Add project files.

---
+++ StringExtensionsLibrary/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace StringExtensionsLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return false; 
+            }
+
+            return char.IsUpper(str[0]);
+        }
+    }
+}
